04e2ffe46b3b63aec42423927c9e4517a3118110
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2022-03-22 12:52:56 -0400

    Initial Commit

---
+++ Testing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testing
+//
+//  Created by Quin’darius Lyles-Woods on March/22/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Quin’darius Lyles-Woods on March/22/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
